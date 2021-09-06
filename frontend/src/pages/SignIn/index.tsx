c331428fde6a9fb8d0172af316b841b9d122b498
JosueFS <josue.18rj@gmail.com>
2021-09-06 04:20:29 -0300

    feat: handle signIn and redirect to Home page

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef, useCallback, FormEvent } from 'react';
 import { FaUserGraduate } from 'react-icons/fa';
 
 import * as S from './styles';
 import logoJG from '../../assets/logo.jpeg';
 import Input from '../../components/Input';
 
+import { useAuth } from '../../hooks/auth';
+
 const SignIn: React.FC = () => {
+  const { signIn } = useAuth();
+
+  const inputName = useRef<HTMLInputElement>({} as HTMLInputElement);
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+
+      if (inputName.current !== null) {
+        await signIn(inputName.current.value);
+      }
+    },
+    [signIn],
+  );
   return (
     <S.Container>
       <S.LogoContainer>
@@ -13,9 +29,10 @@ const SignIn: React.FC = () => {
         <h2>Sala dos Gênios</h2>
       </S.LogoContainer>
 
-      <S.Form>
+      <S.Form onSubmit={handleSubmit}>
         <h4>Acessar sala de aula</h4>
         <Input
+          ref={inputName}
           name="student-name"
           type="text"
           placeholder="Digite seu nome"
