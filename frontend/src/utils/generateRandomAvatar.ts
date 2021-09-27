bff1cfca1bda401e73e553e3067732054d6453f6
JosueFS <josue.18rj@gmail.com>
2021-09-27 07:47:46 -0300

    feat: AvatarGenerate > added user token as payload

--- frontend/src/utils/generateRandomAvatar.ts
+++ frontend/src/utils/generateRandomAvatar.ts
@@ -1,5 +1,5 @@
 import { v4 as uuid } from 'uuid';
 
-export const generateRandomAvatar = (): string => {
-  return `https://avatars.dicebear.com/api/personas/${uuid()}.svg`;
+export const generateRandomAvatar = (userToken = uuid()): string => {
+  return `https://avatars.dicebear.com/api/personas/${userToken}.svg`;
 };
